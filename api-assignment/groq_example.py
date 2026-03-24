5dd45ab7366dd532c467c00a617481c00ee2fb6b
Sanj244 <134857742+Sanj244@users.noreply.github.com>
2026-03-24 23:36:47 +0530

    Refactor get_groq_response function for clarity

--- api-assignment/groq_example.py
+++ api-assignment/groq_example.py
@@ -1,40 +1,26 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq
-
 load_dotenv()
-
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
-
 def get_groq_response(prompt):
     try:
         completion = client.chat.completions.create(
-            model="llama-3.1-8b-instant",  # ✅ use from your list
+            model="llama-3.1-8b-instant",  
             messages=[
                 {"role": "user", "content": prompt}
             ],
             temperature=0.7
         )
-
         return completion.choices[0].message.content
-
     except Exception as err:
         return f"Error occurred: {err}"
-
-
 def main():
     print("\nGroq API Program")
-
     prompt = input("Enter your prompt: ")
-
     print("\nQuerying\n")
-
     result = get_groq_response(prompt)
-
     print("AI Response:\n")
     print(result)
-
-
 if __name__ == "__main__":
     main()
